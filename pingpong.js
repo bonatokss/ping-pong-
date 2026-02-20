47666bf3cad44777209efe73a4bb6a22c208ae7e
bonatokss <joao.bonato.santos@escola.pr.gov.br>
2026-02-20 07:48:55 -0300

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,30 @@
+//criar bolinha 
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
+
+// velocidade da Bolinha 
+let velocidadeXBolinha = 6;
+let velociodadeYBolinha = 6;
+
+//Minha Raquete 
+let xRaquete = 5;
+let yRaquete = 5;
+
+// Raquete Oponente 
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+// Placar
+let meusPontos = 0;
+let pontosOponente = 0;
+
+function setup(){
+  createCanvas(600,400);
+}
+
+function draw(){
+  
+}
+
